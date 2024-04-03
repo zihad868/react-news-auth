9cfb67620af1974d327f8e446842d7ad95bba6ad
zihad868 <zihadh868@gmail.com>
2024-04-03 21:29:21 +0600

    Tailwind, DaisyUi & firebase setup

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCJSx0B60RDwPVBvSgipNDC8V9Sj1DRE48",
+  authDomain: "react-news-auth-83154.firebaseapp.com",
+  projectId: "react-news-auth-83154",
+  storageBucket: "react-news-auth-83154.appspot.com",
+  messagingSenderId: "238910200709",
+  appId: "1:238910200709:web:0011aefa3fb54fc164c2ec"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
